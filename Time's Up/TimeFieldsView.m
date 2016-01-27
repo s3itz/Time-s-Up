d04d65f13f353dce5a4cc7657757a9800feb79bf
Daniel Seitz <daniel.e.seitz@gmail.com>
2016-01-27 09:01:55 -0600

    I wish I could remember what I had changed

--- Time's Up/TimeFieldsView.m
+++ Time's Up/TimeFieldsView.m
@@ -102,7 +102,7 @@
 #pragma mark - Protocol conformance
 #pragma mark - NSTextFieldDelegate
 
-- (void)controlTextDidChange:(NSNotification *)obj {
+- (void):(NSNotification *)obj {
     static NSCharacterSet *numericSet = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
